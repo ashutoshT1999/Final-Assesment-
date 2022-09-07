bc9dc5f0536009f9deecc1908166543844fd9db4
ashutoshT1999 <atiwariiit@gmail.com>
2022-09-07 13:24:03 +0530

    Final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EditcandidateComponent } from './edit candidate/editcandidate.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { personformdeactivate } from './employee-service/employee-data-service';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
